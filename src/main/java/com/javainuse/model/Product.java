4ba5d0d50c8177424003348a5ebcc345a2acf62b
nageshwar123 <nagesh.bandi97@gmail.com>
2018-02-21 12:08:21 -0600

    firstcommit

---
+++ src/main/java/com/javainuse/model/Product.java
@@ -0,0 +1,22 @@
+package com.javainuse.model;
+
+public class Product {
+
+	private int type;
+	private String discount;
+	public int getType() {
+		return type;
+	}
+	public void setType(int type) {
+		this.type = type;
+	}
+	public String getDiscount() {
+		return discount;
+	}
+	public void setDiscount(String discount) {
+		this.discount = discount;
+	}
+
+	
+
+}
